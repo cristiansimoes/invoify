3e72827cb6052187742776912f1409fba840195f
Cristian Menezes <christianmenezes11@gmail.com>
2025-11-04 19:38:04 -0300

    stripe apiversion for vercel

--- app/api/stripe/portal/route.ts
+++ app/api/stripe/portal/route.ts
@@ -5,7 +5,6 @@ import { clerkClient } from "@clerk/clerk-sdk-node";
 import { headers } from "next/headers";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
 });
 
 export async function POST(req: Request) {
